6083807b8a0d84f6c967901a87de761d2e7b2883
Martinson1252 <martinson1202@gmail.com>
2024-04-19 20:52:55 +0200

    .

--- src/App.js
+++ src/App.js
@@ -49,13 +49,14 @@ function App() {
     
     <div className="container mx-auto">
       
-      <div className=' max-w-sm rounded overflow-hidden my-10 mx-auto'>
+      <div class='sticky top-10 max-w-sm rounded  my-10 mx-auto bg-white'>
+      {/* <div class='sticky top-0 w-auto max-w-sm rounded overflow-hidden my-10 mx-auto bg-white'> */}
       <form onSubmit={()=>setTerm(query)} className="w-full max-w-sm">
         <div className="flex items-center border-b border-b-2 border-purple-500 py-2">
         <input onChange={e => setQuery(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Term..." />
-        <button className="flex-shrink-0 bg-purple-500 hover:bg-purple-800 border-purple-500 hover:text-white hover:border-purple-800 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+        {/* <button className="flex-shrink-0 bg-purple-500 hover:bg-purple-800 border-purple-500 hover:text-white hover:border-purple-800 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
       Search
-      </button>
+      </button> */}
       </div>
       </form>
 		</div>
